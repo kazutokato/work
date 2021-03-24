7b700f94b97744719beed03ee0218e90c56e0443
kazutokato <e.t.s.w.a.n.a@gmail.com>
2021-03-24 23:40:15 +0900

    Add udemy.lesson.59

---
+++ ud-js/ls.59/main.js
@@ -0,0 +1,23 @@
+const todos = [
+  {
+    id: 1,
+    title: 'Go to hospital',
+    complited: true
+  },
+  {
+    id: 2,
+    title: 'Go to concert',
+    complited: false
+  },
+  {
+    id: 3,
+    title: 'Go to eat',
+    complited: true
+  }
+]
+
+for(let todo of todos) {
+  if(todo.complited === true){
+    console.log(todo.title);
+  }
+}
